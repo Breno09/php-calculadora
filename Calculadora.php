cf19bd8f1f8a967cdf5841b085c8e4d5682ce8fd
Breno Brandao <breno_brandao12@hotmail.com>
2021-07-26 20:58:08 -0300

    arquivos adicionais inicial

---
+++ Calculadora.php
@@ -0,0 +1,44 @@
+<?php
+
+    class Calculadora
+    {
+        public function soma($x, $y)
+        {
+            $soma = $x + $y;
+            return $soma;
+        }
+
+        public function sub($x, $y)
+        {
+            return $x - $y;
+        }
+
+        public function mult($x, $y)
+        {
+            return $x * $y;
+        }
+
+        public function div($x, $y)
+        {
+            return $x / $y;
+        }
+
+        public function dobro($x, $y)
+        {
+            return $x *2;
+        }
+
+        public function pot($x, $y)
+        {
+            $result = 1;
+            for ($i = 0; $i < $y; $i++) {
+                $result = $result * $x;
+            }
+            return $result;
+        }
+    }
+
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php 
+
+    include('Calculadora.php');
+
+    $calculadora = new Calculadora();
+
+    echo 'Pot: ' . $calculadora->pot(10, 8);
+
+
+?>
